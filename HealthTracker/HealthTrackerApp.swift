488afc35c156fba14621834583f6d4daf645432d
Arjun Dubey <arjundubey0084@gmail.com>
2025-09-02 13:57:47 +0530

    Initial Commit

---
+++ HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Arjun Dubey on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthTrackerTests/HealthTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerTests.swift
+//  HealthTrackerTests
+//
+//  Created by Arjun Dubey on 02/09/25.
+//
+
+import Testing
+@testable import HealthTracker
+
+struct HealthTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
